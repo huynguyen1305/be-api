459b492e9e677380e55c98be0c0bcb73ed0cf3f0
Nguyen Ngoc Huy <huynguyen1305@nexondv.com>
2024-03-25 11:15:51 +0700

    update src

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,8 @@ app.use(
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   }),
 );
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(session({ secret: 'ongbatoi.vn', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
